301d2ad4a8eaf3dfab0201345992066ce982becb
fmm151 <94303897+fmm151@users.noreply.github.com>
2024-07-10 18:45:33 +0300

    Update multiclass.py

--- src/random_forest_xai/multiclass.py
+++ src/random_forest_xai/multiclass.py
@@ -138,7 +138,7 @@ def oversample_data(X_train, y_train):
 
 
 def train_model(X_train, y_train):
-    model = RandomForestClassifier(n_estimators=10, max_depth=10, n_jobs=-1, random_state=RNG)
+    model = RandomForestClassifier(n_estimators=100, max_depth=100, n_jobs=-1, random_state=RNG)
     model.fit(X_train, y_train.values.ravel())
 
     return model
